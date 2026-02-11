5563a80c57f6bfaf46d09286630d19dadee4c051
Lucio Moraes <luciomoraes@gmail.com>
2026-02-11 21:58:57 +0100

    - adding output (io.Writer) - using fmt.Fprintf

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -1,28 +1,44 @@
 package pocketlog
 
+import (
+	"fmt"
+	"io"
+)
+
 // Logger is used to log information.
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // New returns you a logger, ready to log at the required threshold.
-func New(threshold Level) *Logger {
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
 // Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	// implement me
+	if l.threshold > LevelDebug {
+		return
+	}
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
 
 // Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
-	// implement me
+	if l.threshold > LevelInfo {
+		return
+	}
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
 
 // Errorf formats and prints a message if the log level is error or higher.
 func (l *Logger) Errorf(format string, args ...any) {
-	// implement me
+	if l.threshold > LevelError {
+		return
+	}
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
--- pocketlog/logger_test.go
+++ pocketlog/logger_test.go
@@ -1,9 +1,13 @@
 package pocketlog_test
 
-import "github.com/luciormoraes/logger-go/pocketlog"
+import (
+	"os"
+
+	"github.com/luciormoraes/logger-go/pocketlog"
+)
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
 }
